82bb2ecb11cb4793dbe7eb20099bc6681b384b47
조경진 <cho881020@gmail.com>
2021-08-24 11:02:41 +0900

    뷰페이저 + 프래그먼트 하나 생성

---
+++ app/src/main/java/com/neppplus/viewpager/fragments/HelloFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.viewpager.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager.R
+
+class HelloFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        달리 이벤트처리 코드 없다.
+
+    }
+
+
+}
